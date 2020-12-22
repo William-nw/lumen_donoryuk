faea363392fc4e0ebffac90d674ed1ce71763d52
William-nw <65323362+William-nw@users.noreply.github.com>
2020-12-22 11:36:56 +0700

    add nullable on gender

--- database/migrations/2020_12_22_042513_alter_table_gender.php
+++ database/migrations/2020_12_22_042513_alter_table_gender.php
@@ -14,7 +14,7 @@ class AlterTableGender extends Migration
     public function up()
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->string('gender');
+            $table->string('gender')->nullable();
         });
     }
 
